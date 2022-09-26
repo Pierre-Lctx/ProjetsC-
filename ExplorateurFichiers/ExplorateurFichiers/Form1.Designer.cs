9dc3f12204813917b05131090a2ec26d5821b5ef
Pierre-Lctx <pierre.lechatreux@hotmail.fr>
2022-09-26 22:42:47 +0200

    Explorateur de fichier
    V1.0
    
    - Création de l'explorateur de fichiers
    - Ouverture simple d'un fichier avec un bouton open qui appelle une fenêtre de dialogue
    - Ouverture simple d'un fichier avec le chemin d'accès au ficher

---
+++ ExplorateurFichiers/ExplorateurFichiers/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ExplorateurFichiers
+{
+    partial class FileBrowser
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openButton = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(1097, 826);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Ouvrir";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.imageList;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(1160, 808);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 832);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chemin du fichier :";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(112, 829);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(979, 20);
+            this.textBoxPath.TabIndex = 3;
+            // 
+            // FileBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 861);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.openButton);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "FileBrowser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Explorateur de fichiers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+    }
+}
+
---
+++ ExplorateurFichiers/ExplorateurFichiers/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExplorateurFichiers
+{
+    public partial class FileBrowser : Form
+    {
+        List<string> listFiles = new List<string>();
+        public FileBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //Nous nettoyons la liste
+            listFiles.Clear();
+
+            //Nous nettoyons la listView
+            listView.Items.Clear();
+
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Selectionnez votre fichier." })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxPath.Text = fbd.SelectedPath;
+                    foreach(string item in Directory.GetFiles(fbd.SelectedPath))
+                    {
+                        imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                        FileInfo fi = new FileInfo(item);
+                        listFiles.Add(fi.FullName);
+                        listView.Items.Add(fi.Name, imageList.Images.Count - 1);
+                    }
+                }
+            }
+        }
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                Process.Start(listFiles[listView.FocusedItem.Index]);
+            }
+        }
+    }
+}
